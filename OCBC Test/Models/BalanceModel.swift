c814ab40ccaf1e31bea388b10580b051f4ebfd8f
Kevin Chilmi rezhaldo <85718279+Rezhaldo@users.noreply.github.com>
2022-05-22 11:48:52 +0700

    LoginAPI
    Login

---
+++ OCBC Test/Models/BalanceModel.swift
@@ -0,0 +1,8 @@
+//
+//  BalanceModel.swift
+//  OCBC Test
+//
+//  Created by Kevin Chilmi Rezhaldo on 22/05/22.
+//
+
+import Foundation
--- OCBC Test/Views/LoginViewController.swift
+++ OCBC Test/Views/LoginViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Alamofire
 
 class LoginViewController: UIViewController, UITextFieldDelegate {
     
@@ -34,10 +35,10 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
     
     @IBAction func buttonLoginInTapped(_ sender: Any) {
         if textFieldPassword.hasText && textFieldUsernameEntry.hasText {
-            let storyBoard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
-            let dashboardViewController = storyBoard.instantiateViewController(withIdentifier: "dashboard") as! DashboardViewController
-            dashboardViewController.modalPresentationStyle = .fullScreen
-            self.present(dashboardViewController, animated: true, completion: nil)
+//            let storyBoard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+//            let dashboardViewController = storyBoard.instantiateViewController(withIdentifier: "dashboard") as! DashboardViewController
+//            dashboardViewController.modalPresentationStyle = .fullScreen
+//            self.present(dashboardViewController, animated: true, completion: nil)
         }
         
         if textFieldUsernameEntry.hasText{
@@ -51,6 +52,48 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
         }else{
             labelAlertPassword.isHidden = false
         }
+        
+        let loginRequest = LoginRequest(username: textFieldUsernameEntry.text ?? "", password: textFieldPassword.text ?? "")
+        let header = HTTPHeaders(["Content-Type":"application/json", "Accept":"application/json"])
+        let loginUrl = URL(string: "https://green-thumb-64168.uc.r.appspot.com/login")
+//        Alamofire.Session().request(loginUrl, method: .post, parameters: loginRequest, encoder: JSONEncoding.default, headers: "").responseJSON(completionHandler: { response in
+//
+//        } )
+        
+        AF.request(loginUrl!, method: .post, parameters: loginRequest, encoder: JSONParameterEncoder.default, headers: header).response(completionHandler: { response in
+//            print("response \(response.debugDescription)")
+            guard let data = response.data else { return }
+            
+            do {
+                let decoder = JSONDecoder()
+                let loginResponse = try decoder.decode(LoginResponse.self, from: data)
+                print("Success request: \(loginResponse)")
+                
+                let storyBoard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+                let dashboardViewController = storyBoard.instantiateViewController(withIdentifier: "dashboard") as! DashboardViewController
+                dashboardViewController.modalPresentationStyle = .fullScreen
+                self.present(dashboardViewController, animated: true, completion: nil)
+                
+            } catch let error {
+                print("Error request: \(error.localizedDescription)")
+            }
+        })
+        
+//        Alamofire.Session().request(loginUrl!, method: .post, parameters: loginRequest, encoder: JSONParameterEncoder.default, headers: header).response(completionHandler: { response in
+//            print("response \(response.debugDescription)")
+//            guard let data = response.data else { return }
+//
+//            do {
+//                let decoder = JSONDecoder()
+//                let loginResponse = try decoder.decode(LoginResponse.self, from: data)
+//                print("Success request: \(loginResponse)")
+//
+//            } catch let error {
+//                print("Error request: \(error.localizedDescription)")
+//            }
+//        })
+      
+        
     }
     
     
